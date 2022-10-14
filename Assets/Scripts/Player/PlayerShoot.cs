5ef45ce731f5c168e6cf71a6bdd49514f01e7f0d
Vinicius Santos <viniciusantos0898@gmail.com>
2022-10-14 11:06:02 -0300

    (Revision) added fire rate to player´s single and triple shot

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -4,19 +4,55 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    [Header("Script References")]
     [SerializeField] private BulletPooling bulletPool;
+
+    [Header("Bullet Spawns")]
     [SerializeField] private Transform frontalBulletSpawn;
-    [SerializeField] private Transform[] lateralBulletSpawns = new Transform[3]; 
-    
+    [SerializeField] private Transform[] lateralBulletSpawns = new Transform[3];
+
+    [Header("Fire Rates")]
+    [SerializeField] private float singleShotFireRate;
+    [SerializeField] private float tripleShotFireRate;
+
+    private float nextSingleShot;
+    private float nextTripleShot;
+
+    private void Start()
+    {
+        nextSingleShot = singleShotFireRate;
+        nextTripleShot = tripleShotFireRate; 
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        nextSingleShot -= Time.deltaTime;
+        nextTripleShot -= Time.deltaTime;
+
         if (Input.GetMouseButtonDown(0))
-            bulletPool.SingleBulletSpawn(frontalBulletSpawn, true);
+            SingleShot();
 
         if (Input.GetMouseButtonDown(1))
-            bulletPool.MultipleBulletSpawn(lateralBulletSpawns, true);
+            MultipleShot();
+    }
 
+    private void SingleShot() 
+    {
+        if(nextSingleShot <= 0f)
+        {
+            bulletPool.SingleBulletSpawn(frontalBulletSpawn, true);
+            nextSingleShot = singleShotFireRate;
+        }
+    }
+
+    private void MultipleShot() 
+    { 
+        if(nextTripleShot <= 0f)
+        {
+            bulletPool.MultipleBulletSpawn(lateralBulletSpawns, true);
+            nextTripleShot = tripleShotFireRate;
+        }
     }
 }
